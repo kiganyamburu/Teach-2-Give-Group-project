17f2cade5bd87361aafdc93b84145b1795d9f19b
Victor Preston <112781610+victorpreston@users.noreply.github.com>
2024-06-11 14:03:30 +0300

    backend directory

--- backend/src/interfaces/RequestWithUser.ts
+++
@@ -1,9 +0,0 @@
-// src/interfaces/RequestWithUser.ts
-import { Request } from 'express';
-
-export interface RequestWithUser extends Request {
-  user?: {
-    id: string;
-    role: string;
-  };
-}
